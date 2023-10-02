587b136f76a8360fbeec34572939b9f3172bc6a2
Michael Plaksyuk <mplaksyuk@fire>
2023-10-03 01:08:10 +0300

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,24 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Input from "../Input/Input";
+
+import "./Header.css";
+
 export default function Header() {
 	return (
 		<>
 			<header>
-				<span>123</span>
+				<Navbar>
+					<Navbar.Brand href="/" className="brand">JIPS</Navbar.Brand>
+					<Nav.Link href="/">New Task</Nav.Link>
+					<Nav.Link href="/">Project</Nav.Link>
+					<Nav.Link href="/">Dashboards</Nav.Link>
+					<Nav.Link href="/">Teams</Nav.Link>
+					<Nav.Link href="/">Messages</Nav.Link>
+				</Navbar>
+				<div className="header__controls">
+					<Input placeholder="Search for project" />
+				</div>
 				{/* Brand */}
 				{/* Navbar */}
 				{/* Controls */}
