7bdb3ac3e5f4204af7fc2bd3669bb603dff09134
Michael Plaksyuk <mplaksyuk@fire>
2023-10-09 20:41:31 +0300

    Authorization

---
+++ src/pages/Authorization/Registration.jsx
@@ -0,0 +1,24 @@
+import './Registration.css'
+import {Container, Form} from "react-bootstrap";
+import Input from "../../components/Input/Input";
+import Button from "../../components/Button/Button";
+export default function Registration() {
+    return(
+        <Container>
+            <div className="authorization">
+                <h3>Authorization form</h3>
+                <Form className="form">
+                    <Form.Group >
+                        <Form.Label>Login</Form.Label>
+                        <div><Input /></div>
+                    </Form.Group>
+                    <Form.Group >
+                        <Form.Label>Password</Form.Label>
+                        <div><Input /></div>
+                    </Form.Group>
+                    <Button text="Submit" />
+                </Form>
+            </div>
+        </Container>
+    )
+}
