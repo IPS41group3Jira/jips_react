6e43dde7dc2fea56f90b9ba2f51591f1e2a23036
Enotik98 <poliakitti@gmail.com>
2023-10-16 23:37:28 +0300

    change authorization form

--- src/pages/Authorization/Login.jsx
+++ src/pages/Authorization/Login.jsx
@@ -1,23 +1,44 @@
 import './Authorization.css'
-import { Container, Form } from "react-bootstrap";
-import Input from "../../components/Input/Input";
+import {Container, Form} from "react-bootstrap";
 import Button from "../../components/Button/Button";
+import {Link} from "react-router-dom";
+import {useState} from "react";
+import {FaEye, FaEyeSlash} from "react-icons/fa";
+
 export default function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    }
     return (
         <>
             <Container>
                 <div className="authorization">
-                    <label>Authorization form</label>
+                    <label>Log in</label>
                     <Form className="authorization-form" name="login-form">
-                        <Form.Group >
-                            <Form.Label>Login</Form.Label>
-                            <Input />
+                        <Form.Group>
+                            <input type="text" placeholder="Username" value={username} onChange={(e)=>setUsername(e.target.value)} className="authorization-input"/>
                         </Form.Group>
-                        <Form.Group >
-                            <Form.Label>Password</Form.Label>
-                            <Input />
+                        <Form.Group className="password-group">
+                                <input
+                                    type={showPassword ? 'text' : "password"}
+                                    value={password}
+                                    placeholder="Password"
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    className="authorization-input"/>
+                                {showPassword ? (
+                                    <FaEyeSlash onClick={togglePasswordVisibility} className="eye-icon"/>
+                                ) : (
+                                    <FaEye onClick={togglePasswordVisibility} className="eye-icon"/>
+                                )}
+                            <span>Forgot password?</span>
+                            <Form.Check inline label="Remember me" type="checkbox" className="authorization-check"/>
                         </Form.Group>
-                        <Button text="Submit" />
+                        <label>First time here? <Link to="/registration" className="authorization-link">Sign
+                            up</Link></label>
+                        <Button text="Submit"/>
                     </Form>
                 </div>
             </Container>
--- src/pages/Authorization/Registration.jsx
+++ src/pages/Authorization/Registration.jsx
@@ -1,35 +1,51 @@
 import './Authorization.css'
 import {Container, Form} from "react-bootstrap";
-import Input from "../../components/Input/Input";
 import Button from "../../components/Button/Button";
+import {Link} from "react-router-dom";
+import {FaEye, FaEyeSlash} from "react-icons/fa";
+import {useState} from "react";
 
 export default function Registration() {
+    const [username, setUsername] = useState("")
+    const [first_name, setFirst_name] = useState("")
+    const [last_name, setLast_name] = useState("")
+    const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    }
     return (
         <>
             <Container>
                 <div  className="authorization">
-                    <label>Registration form</label>
+                    <label>Create an account</label>
                     <Form className="authorization-form" name="register-form">
                         <Form.Group >
-                            <Form.Label>Login</Form.Label>
-                            <Input />
+                            <input type="text" value={username} onChange={(e)=>setUsername(e.target.value)} placeholder="Username" className="authorization-input"/>
                         </Form.Group>
 
                         <Form.Group >
-                            <Form.Label>Password</Form.Label>
-                            <Input />
+                            <input type="text" value={first_name} onChange={(e)=>setFirst_name(e.target.value)} placeholder="First name" className="authorization-input"/>
                         </Form.Group>
 
                         <Form.Group >
-                            <Form.Label>First name</Form.Label>
-                            <Input />
+                            <input type="text" value={last_name} onChange={(e)=>setLast_name(e.target.value)} placeholder="Last name" className="authorization-input"/>
                         </Form.Group>
 
-                        <Form.Group >
-                            <Form.Label>Last name</Form.Label>
-                            <Input />
+                        <Form.Group className="password-group">
+                            <input
+                                type={showPassword ? 'text' : "password"}
+                                value={password}
+                                placeholder="Password"
+                                onChange={(e) => setPassword(e.target.value)}
+                                className="authorization-input"/>
+                            {showPassword ? (
+                                <FaEyeSlash onClick={togglePasswordVisibility} className="eye-icon"/>
+                            ) : (
+                                <FaEye onClick={togglePasswordVisibility} className="eye-icon"/>
+                            )}
                         </Form.Group>
-
+                        <label>Already have an account? <Link to="/login" className="authorization-link">Sign in</Link></label>
                         <Button text="Submit" />
                     </Form>
                 </div>
