1f25f232619069cb4b292c4bb60cc6052127c1da
Michael Plaksyuk <mplaksyuk@fire>
2023-11-13 23:33:50 +0200

    config

--- src/Axios.js
+++ src/Axios.js
@@ -18,38 +18,4 @@ Axios.interceptors.request.use(config => {
     return config;
 });
 
-// Axios.interceptors.response.use((response) => response,
-//     (error) => {
-//         const { response, config } = error;
-
-//         if (response.status === 401) {
-//             let refreshToken = localStorage.getItem("refreshToken");
-            
-//             if (refreshToken) {
-//                 const refreshConfig = {
-//                     headers: {
-//                         "Refresh": `Bearer ${refreshToken}`
-//                     }
-//                 }
-//                 console.log('refresh tokens');
-
-//                 const tokens = Axios.post(`${baseurl}tokens/refresh`, {}, refreshConfig).then((tokens) => {
-//                     console.log('refreshed tokens !!!!!');
-//                     let newAccessToken = tokens.data.accessToken;
-//                     let newRefreshToken = tokens.data.refreshToken;
-    
-//                     if (newAccessToken && newRefreshToken) {
-//                         localStorage.setItem("accessToken", newAccessToken);
-//                         localStorage.setItem("refreshToken", newRefreshToken);
-    
-//                         config.headers["Authorization"] = `Bearer ${newAccessToken}`;
-//                         return Axios(config);
-//                     }
-//                     return Promise.reject(error);
-//                 })
-//             }
-//         }
-//     }
-// );
-
 export default Axios;
