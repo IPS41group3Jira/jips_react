83c5b0cf84e6eaf9cd389f05320b91261a69aca3
Michael Plaksyuk <mplaksyuk@fire>
2023-11-27 19:12:02 +0200

    axios setup

--- src/Axios.js
+++ src/Axios.js
@@ -13,6 +13,31 @@ Axios.interceptors.request.use(config => {
         const accessToken = localStorage.getItem("accessToken");
         if(accessToken)
             config.headers["Authorization"] = `Bearer ${accessToken}`;
+
+        // const handleData = (data) => {
+        //     if (typeof data == "object") {
+        //         if (Array.isArray(data)) {
+        //             data.map((obj) => handleData(obj))
+        //         }
+        //         else {
+        //             Object.entries(data).map(([key, value]) => {
+        //                 handleData(value);
+        //                 if (/^(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z)$/.test(value)) { //type date
+        //                     value = new Date(value);
+        //                     const year = value.getFullYear();
+        //                     const month = value.getMonth() + 1;
+        //                     const day = value.getDate();
+        //                     data[key] = `${year}-${month}-${day}`;
+        //                 }
+        //             });
+        //         }
+        //     }
+        // }
+
+        // if (config.data) {
+        //     handleData(config.data);
+        // }
+
     }
 
     return config;
@@ -20,6 +45,26 @@ Axios.interceptors.request.use(config => {
 
 Axios.interceptors.response.use(
     (response) => {
+        // const handleData = (data) => {
+        //     if (typeof data == "object") {
+        //         if (Array.isArray(data)) {
+        //             data.map((obj) => handleData(obj))
+        //         }
+        //         else {
+        //             Object.entries(data).map(([key, value]) => {
+        //                 handleData(value);
+        //                 if (/^(\d{4}-\d{2}-\d{2})$/.test(value)) { //type date
+        //                     data[key] = new Date(value);
+        //                 }
+        //             });
+        //         }
+        //     }
+        // }
+        
+        // if (response.data) {
+        //     handleData(response.data);
+        // }
+
         return response;
     },
     (error) => {
