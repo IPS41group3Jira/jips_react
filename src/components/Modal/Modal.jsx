3c6eeca137258056cce6b8d8a58ac81ddf8b772d
Michael Plaksyuk <mplaksyuk@fire>
2023-11-28 19:11:29 +0200

    fixed modal scroll

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,13 +3,24 @@ import {AiOutlineClose} from "react-icons/ai";
 import './Modal.css';
 
 export default function Modal({ isOpen, onClose, children }) {
-    if (!isOpen) return null;
+    if (!isOpen) {
+        return null;
+    }
+
+    const onCloseHandle = (e) => {
+        document.body.style.overflow = null;
+        if (typeof onClose === 'function') {
+            onClose(e);
+        }
+    }
+
+    document.body.style.overflow = 'hidden';
 
     return (
         <div className="ui-modal-wrapper" >
             <div className='ui-modal'>
                 <div className='ui-modal__header'>
-                    <button onClick={onClose}><AiOutlineClose/></button>
+                    <button onClick={onCloseHandle}><AiOutlineClose/></button>
                 </div>
                 <div className='ui-modal__content'>
                     {children}
